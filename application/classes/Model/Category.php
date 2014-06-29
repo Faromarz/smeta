860fe26b20002eef1c175698d0da51d2677b7df7
senj <senj@mail.ru>
2014-06-29 17:14:07 +0300

    admin

--- application/classes/Model/Category.php
+++ application/classes/Model/Category.php
@@ -2,6 +2,14 @@
 
 class Model_Category extends ORM_MPTT
 {
-         protected $_table_name = 'categories';
-         protected $_primary_key = 'id';
+
+    protected $_table_name = 'categories';
+    protected $_primary_key = 'id';
+
+    public function getEmptyLvl()
+    {
+        return str_repeat(
+                html_entity_decode('&nbsp;', ENT_QUOTES, 'UTF-8'), ($this->lvl + 1) * 3
+            );
+    }
 }
