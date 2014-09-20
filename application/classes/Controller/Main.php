bd504460eedae40d317e8685bfea17a3be960fe9
senj <senj@mail.ru>
2014-09-20 10:47:50 +0300

    new.mastersmeta.ru

--- application/classes/Controller/Main.php
+++ application/classes/Controller/Main.php
@@ -2,72 +2,32 @@
 
 defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Main extends Controller_Core {
+class Controller_Main extends Controller_Core
+{
 
-    public function action_index() {
+    public function action_index()
+    {
         $types_rate = ORM::factory('Types_Rate')->find_all();
         $types_repair = ORM::factory('Types_Repair')->find_all();
         $types_apartment = ORM::factory('Types_Apartment')->find_all();
+        
 
         $this->set('types_rate',$types_rate);
         $this->set('types_repair',$types_repair);
         $this->set('types_apartment',$types_apartment);
+        
+        $rooms = ORM::factory('Room')->order_by('id', 'ASC')->find_all();
+        $_rooms = array();
+        foreach ($rooms as $key => $room) {
+            $_rooms[$key] = $room->as_array();
+        }
+        $this->set('_rooms', $_rooms);
 
-      /*  $materials_old = ORM::factory('Matoldtypes')->where('parent_id','=','0')->find_all();
-
-        foreach ($materials_old as $types_p){
-            $material_type = ORM::factory('Material_Type');
-            $material_type -> name = $types_p->name;
-            $material_type ->save();
-            $materials_old_species = ORM::factory('Matoldtypes')->where('parent_id','=',$types_p->id)->find_all();
-            if (ORM::factory('Matoldtypes')->where('parent_id','=',$types_p->id)->count_all()>0){
-                foreach ($materials_old_species as $species_p){
-                    $material_species = ORM::factory('Material_Species');
-                    $material_species -> type_id = $material_type->id;
-                    $material_species -> name = $species_p->name;
-                    $material_species ->save();
-
-                    $materials_old = ORM::factory('Materialold')->where('category_id','=',$species_p->id)->find_all();
-                    foreach ($materials_old as $material_old){
-                        $material = ORM::factory('Material');
-                        $material -> species_id = $material_species->id;
-                        $material -> name = $material_old->name;
-                        $material -> country_id = $material_old->country_id;
-                        $material -> description = $material_old->description;
-                        $material -> img = $material_old->img;
-                        $material -> price = $material_old->price;
-                        $material -> count_text = $material_old->count_text;
-                        $material -> create_date = $material_old->create_date;
-                        $material ->save();
-                    }
-                }
-            }
-            else{
-                $material_species = ORM::factory('Material_Species');
-                $material_species -> type_id = $material_type->id;
-                $material_species -> name = '';
-                $material_species ->save();
-
-                $materials_old = ORM::factory('Materialold')->where('category_id','=',$types_p->id)->find_all();
-                foreach ($materials_old as $material_old){
-                    $material = ORM::factory('Material');
-                    $material -> species_id = $material_species->id;
-                    $material -> name = $material_old->name;
-                    $material -> country_id = $material_old->country_id;
-                    $material -> description = $material_old->description;
-                    $material -> img = $material_old->img;
-                    $material -> price = $material_old->price;
-                    $material -> count_text = $material_old->count_text;
-                    $material -> create_date = $material_old->create_date;
-                    $material ->save();
-                }
-
-            }
-
-
-        }*/
-
-
+        $params = ORM::factory('Roomparamsdef')->order_by('id', 'ASC')->find_all();
+        $_params = array();
+        foreach ($params as $key => $param) {
+            $_params[$key] = $param->as_array();
+        }
+        $this->set('_params', $_params);
     }
-
 }
--- application/data/_rooms.sql
+++ application/data/_rooms.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.7deb7
+-- version 4.2.8
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Время создания: Сен 13 2014 г., 15:26
--- Версия сервера: 5.1.73
--- Версия PHP: 5.3.3
+-- Время создания: Сен 15 2014 г., 23:12
+-- Версия сервера: 5.5.38-MariaDB-wsrep
+-- Версия PHP: 5.5.16
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -26,27 +27,50 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 
 CREATE TABLE IF NOT EXISTS `rooms` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `type` int(11) NOT NULL,
   `name` varchar(128) NOT NULL,
   `length` decimal(4,2) NOT NULL,
   `width` decimal(4,2) NOT NULL,
   `balcony` tinyint(1) DEFAULT NULL,
-  `show` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
+  `show` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `rooms`
 --
 
 INSERT INTO `rooms` (`id`, `type`, `name`, `length`, `width`, `balcony`, `show`) VALUES
-(1, 1, 'Комната №1', 5.50, 3.50, 0, 1),
-(2, 1, 'Комната №2', 5.50, 3.50, 0, 0),
-(3, 1, 'Комната №3', 5.50, 3.50, 0, 0),
-(4, 1, 'Комната №4', 5.50, 3.50, 0, 0),
-(5, 1, 'Комната №5', 5.50, 3.50, 0, 0),
-(6, 2, 'Кухня', 3.00, 3.00, 0, 1),
-(7, 3, 'Коридор', 3.00, 2.00, NULL, 1),
-(8, 4, 'Ванна', 1.80, 1.70, NULL, 1),
-(9, 5, 'Туалет', 1.70, 0.80, NULL, 1);
+(1, 1, 'Комната №1', '5.50', '3.50', 0, 1),
+(2, 1, 'Комната №2', '5.50', '3.50', 0, 0),
+(3, 1, 'Комната №3', '5.50', '3.50', 0, 0),
+(4, 1, 'Комната №4', '5.50', '3.50', 0, 0),
+(5, 1, 'Комната №5', '5.50', '3.50', 0, 0),
+(6, 2, 'Кухня', '3.00', '3.00', 0, 1),
+(7, 3, 'Коридор', '3.00', '2.00', NULL, 1),
+(9, 4, 'Ванна', '1.80', '1.70', NULL, 1),
+(10, 5, 'Туалет', '1.70', '0.80', NULL, 1),
+(8, 4, 'Ванна и туалет', '1.80', '1.70', NULL, 0);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `rooms`
+--
+ALTER TABLE `rooms`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `rooms`
+--
+ALTER TABLE `rooms`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
